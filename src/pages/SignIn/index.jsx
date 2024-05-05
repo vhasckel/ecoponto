9e7c4ba25ef46daccb6af4a0ecef0c0a9cd03f1b
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-05-05 19:03:58 -0300

    construindo readme

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React from "react";
 import LoginForm from "../../components/LoginForm";
 
@@ -6,7 +7,33 @@ import styles from "./styles.module.css";
 function SignInPage() {
   return (
     <div className={styles.container}>
-      <div className={styles.backgroundImage}></div>
+      <div className={styles.backgroundImage}>
+        <div>
+          <Link className={styles.logo} to="/">
+            <h3>
+              {" "}
+              Eco<span className={styles.eco}>ponto</span>
+            </h3>
+            <img
+              src="src/assets/logo.png"
+              alt="Logo da plataforma, uma mão estendida com um broto de planta sobre ela"
+            />
+          </Link>
+        </div>
+      </div>
+      <div className={styles.mobileScreen}>
+        <div>
+          <Link className={styles.logo} to="/">
+            <h3>
+              Eco<span className={styles.eco}>ponto</span>
+            </h3>
+            <img
+              src="src/assets/logo.png"
+              alt="Logo da plataforma, uma mão estendida com um broto de planta sobre ela"
+            />
+          </Link>
+        </div>
+      </div>
       <div className={styles.form}>
         <LoginForm />
       </div>
